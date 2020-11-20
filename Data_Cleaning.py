cf3a67a7b6c01837176a2ee268adf0b33d360053
seespinoza <se.espinoza132@gmail.com>
2020-11-19 19:50:48 -0600

    added plot functions

--- Data_Cleaning.py
+++ Data_Cleaning.py
@@ -10,20 +10,61 @@ acc_df = pd.read_csv('Data_Cleaning_Activity_Data.csv')
 acc_df.info()
 
 
-
 # Task 4: Fill down columns
+def fill_down_dates(xdf):
+
+    # Check if there is only one non-NA row
+    if xdf['date_turnover'].count() == 0:
+        print(xdf['date_turnover'])
+        pass
+
+    elif xdf['date_turnover'].count() == 1:
+        xdf['date_turnover'] = xdf['date_turnover'].ffill()
+
+    else:
+        xdf['date_turnover'].fillna(method='ffill', inplace=True)
+
+        # Minimum date
+
+
+        # Keep turnover days less than account_closed, replace ones greater with the minimum account_closed date
+        #xdf['date_turnover'] = np.where((xdf['account_closed'] - xdf['date_turnover']).dt.days > 0, np.NaN,
+                                        #min_date)
+        for row in xdf.itertuples():
+
+            fval = xdf.at[row.Index, 'date_turnover']
+
+            # Change rows that have account_closed dates after date_turnover
+            if (xdf.at[row.Index,'account_closed'] - xdf.at[row.Index, 'date_turnover']).days < 0:
+
+                fval = xdf['date_turnover'].min()
+
+            xdf.at[row.Index, 'date_turnover'] = fval
+
+
+    return xdf['date_turnover']
+
+
 def fill_down(df):
 
+    # Change dates to datetime objects
+    df['account_closed'] = pd.to_datetime(df['account_closed'])
+    df['date_turnover'] = pd.to_datetime(df['date_turnover'])
+
     cols = ['kids_count', 'credit_score', 'returned_checks',
-                           'collection_agency_code', 'date_turnover', 'amount_owed']
+                           'collection_agency_code', 'amount_owed']
 
     # Group by account ID and forward fill data
-    df[cols] = df.groupby(['account_ID'], sort=False)[cols].ffill()
+    df[cols] = df.groupby(['account_ID'], sort=False, as_index=False)[cols].ffill()
+
+    # Fill down dates
+    dfcol = df.groupby(['account_ID'], sort=False).apply(fill_down_dates)
+    df['date_turnover'] = dfcol.reset_index(level=0, drop=True)
 
     return df
 
-
 acc_df = fill_down(acc_df)
+
 
 # Task 5: Conflicting Columns
 # Check if collection_agency_code indeed conflicts w/ agency_code
@@ -43,13 +84,32 @@ acc_df = remove_col_ag_code(acc_df)
 
 c = acc_df.loc[acc_df['account_ID'] == '902429_61468372']
 
+
 # Task 6: Remove a variable that is not needed in the data set
 # Since we already have the agency code we don't need the agency tab
 def remove_agency(df):
+    # Make a dictionary of agency codes and agency
+    agency_key = dict(zip(df.agency_code, df.agency))
+    agency_key_out = {}
 
-    df = df.drop(['agency'], axis=1)
+    for key in agency_key:
+
+        # Remove all nan from dict and print out the codes w/ agency name
+        if type(agency_key[key]) is str:
+
+            # Add keys to new dictionary w/ lists as values
+            if agency_key[key] in agency_key_out:
+                agency_key_out[agency_key[key]].append(key)
+
+            else:
+                agency_key_out.update({agency_key[key]: [key]})
+
+    print(agency_key_out)
+    # Drop the agency column
+    df = df.drop(['agency_code'], axis=1)
 
     return df
+
 
 acc_df = remove_agency(acc_df)
 
@@ -57,27 +117,23 @@ acc_df = remove_agency(acc_df)
 # Task 7: Aggregate data
 def aggregate_data(df):
 
-    # Change dates to datetime objects
-    df['account_closed'] = pd.to_datetime(df['account_closed'])
-    df['date_turnover'] = pd.to_datetime(df['date_turnover'])
-
     # Apply aggregate functions to data
     agg_dict = {'amount_paid': ['sum'], 'kids_count': [pd.Series.mode],
-                'agency_code': [pd.Series.mode], 'date_turnover': [np.min],
-                'account_closed': [np.max]}
-    df_agg = df.groupby(['account_ID'], sort=False, as_index=False).agg(agg_dict)
+                'zip_code': 'last'}
+    df_agg = df.groupby(['account_ID', 'date_turnover', 'account_closed'], sort=False, as_index=False).agg(agg_dict)
 
     # Rename aggregate data frame columns
-    df_agg.columns = ['account_ID', 'amount_paid_total', 'kids_count_mode', 'agency_code_mode',
-                      'date_turnover_min', 'account_closed_max']
-
-    # Drop duplicate account IDs
-    sub_df = df.drop_duplicates(subset=['account_ID'], keep='last')
-
-    sub_df = sub_df.drop(['amount_paid', 'kids_count', 'agency_code'], axis=1)
+    df_agg.columns = ['account_ID', 'date_turnover', 'account_closed', 'amount_paid_total', 'kids_count_mode',
+                      'zip_code']
 
     # Merge aggregate data w/ dropped account IDs
-    x_merge = pd.merge(sub_df, df_agg, left_on='account_ID', right_on='account_ID', how='left')
+    merge_cols = ['account_ID', 'date_turnover', 'account_closed']
+    df = df.drop(['zip_code', 'amount_paid'], axis=1)
+
+    # Left merge aggregated data with the original dataframe
+    x_merge = pd.merge(df_agg, df, how='left', left_on=merge_cols, right_on=merge_cols)
+
+    x_merge = x_merge.drop_duplicates(merge_cols)
 
     return x_merge
 
@@ -87,10 +143,11 @@ acc_df = aggregate_data(acc_df)
 def calc_day_difference(df):
 
     # Get the difference between account closed and date turnover
-    df['time_to_pay'] = df['account_closed_max'] - df['date_turnover_min']
+    df['time_to_pay'] = df['account_closed'] - df['date_turnover']
 
     # Mark NaNs as -1
     df['time_to_pay'] = df['time_to_pay'].fillna('-1')
+    df['account_closed'] = df['account_closed'].fillna('-1')
 
     # Change time_to_pay column to string
     df['time_to_pay'] = df['time_to_pay'].astype(str)
@@ -98,19 +155,18 @@ def calc_day_difference(df):
     # Change days to int
     df['time_to_pay'] = df['time_to_pay'].str.replace('\sdays.*', '').astype(int)
 
-    # Check that no negative day values were calculated
-    print(df.loc[df['time_to_pay'] < -1])
-
     return df
 
 
 def create_day_bins(df):
 
-    # Create bins
-    df['0-30_days'] = np.where((df['time_to_pay'] >= 0) & (df['time_to_pay'] <= 30), 1, 0)
-    #df['31-60_days'] = np.where(df['time_to_pay'] >= 31 & df['time_to_pay'] <= 60, 1, 0)
-    #df['61-90_days'] = np.where(df['time_to_pay'] >= 61 & df['time_to_pay'] <= 90 , 1, 0)
-    #df['90+days'] = np.where(df['time_to_pay'] >= 90, 1, 0)
+    conditions = [(df['time_to_pay'] >= 0) & (df['time_to_pay'] <= 30),
+                                         (df['time_to_pay'] >= 31) & (df['time_to_pay'] <= 60),
+                                         (df['time_to_pay'] >= 61) & (df['time_to_pay'] <= 90),
+                                         df['time_to_pay'] >= 90]
+    intervals = ['0-30 days', '31-60 days', '61-90 days', '90+ days']
+
+    df['pay_time_intervals'] = np.select(conditions, intervals, default='No time interval')
 
     return df
 
@@ -121,5 +177,56 @@ acc_df = create_day_bins(acc_df)
 # Task 9 Create histogram of number of accounts with # of kids
 fig = px.histogram(acc_df, x='kids_count_mode')
 fig.show()
+
+# Task 10
+def top_4_agencies(df):
+    # Plot 1 Data
+    # Get the sum for the total paid amount for each agency
+    groupby_agency = df.groupby(['agency'], sort=False, as_index=False)
+
+    # Get the sum of amount_paid_total for unique accounts for each agency
+    amount_paid_agency = groupby_agency['amount_paid_total'].sum()
+
+    # Sort grouped accounts by amount_paid_total asc
+    amount_paid_agency = amount_paid_agency.sort_values(by=['amount_paid_total'], ascending=False)
+
+    # Get top 4 agencies
+    top4 = amount_paid_agency['agency'].tolist()[:4]
+
+    # Group by agency and time invervals
+    groupby_agency_time = df.groupby(['agency', 'pay_time_intervals'])
+
+    # Total amount paid for each agency for each time interval
+    total_paid_per_time = groupby_agency_time['amount_paid_total'].sum().reset_index()
+
+    # Only get rows that have the top 4 agencies
+    target_rows = total_paid_per_time.agency.isin(top4)
+    filtered_total_paid_per_time = total_paid_per_time[target_rows]
+
+    # Plot 2 Data
+    # Gets the total number of unique accounts (unique ID and date combination)
+    total_accounts = groupby_agency_time.size().reset_index()
+    total_accounts = total_accounts.rename(columns={0: 'total_accounts'})
+
+    # Only get rows that have the top 4 agencies
+    target_rows = total_accounts.agency.isin(top4)
+    filtered_total_accounts = total_accounts[target_rows]
+
+    return filtered_total_paid_per_time, filtered_total_accounts
+
+
+agency_paid, agency_accounts = top_4_agencies(acc_df)
+
+fig10 = px.bar(agency_paid, x='agency', y='amount_paid_total', color='pay_time_intervals', barmode='group',
+               title='Total Money Collected from Top 4 Agencies',
+               labels={'agency': 'Agencies', 'amount_paid_total': 'Amount Paid ($)',
+                       'pay_time_intervals': 'Pay Time Intervals'})
+fig10.show()
+
+fig10_part2 = px.bar(agency_accounts, x='agency', y='total_accounts', color='pay_time_intervals', barmode='group',
+                     title='Total Paying Accounts from Top 4 Agencies',
+                     labels={'agency': 'Agencies', 'total_accounts': 'Number of Accounts',
+                             'pay_time_intervals': 'Pay Time Intervals'})
+fig10_part2.show()
 
 acc_df.to_csv('cleaned_accounts_data.csv')
